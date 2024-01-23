28eec5c4e565ed126e9a416a8cc777f7711b2432
Prachi Gore <prachigore408@gmail.com>
2024-01-23 23:45:20 +0530

    trying to add ball movement

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 document.addEventListener('DOMContentLoaded',()=>{ 
+    const container=document.querySelector('.container')
    //paddle    
 const paddle=document.getElementsByClassName('paddle')  
 let paddleY=1
@@ -12,5 +13,23 @@ const paddleMovement=()=>{
 setInterval(paddleMovement,0.1)
 
 //ball
+const ball=document.querySelector('.ball')
+// console.log(ball.style.left,'ball')
+let ballx=15;
+let bally=1;
+let dx=1
+let dy_ball=1
+const containerWidth=container.offsetWidth
+    console.log(containerWidth,'width')
+const ballMovement=()=>{
+    (ballx<containerWidth-15 && ballx>=15) ? dx*=1 : dx*=-1
+    (bally<500-20 && bally>0) ? dy_ball*=1 : dy_ball*=-1
+    ballx+=dx
+    bally+=dy_ball
+ball.style.left=`${ballx}px`;
+ball.style.top=`${bally}px`;
+
+}
+setInterval(ballMovement,1)
 
 })
