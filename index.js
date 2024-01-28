9b99f6dee17147c1e02bdeb258d3819a1a43e8b4
Prachi Gore <prachigore408@gmail.com>
2024-01-28 21:35:58 +0530

    paddle movement with key added

--- index.js
+++ index.js
@@ -1,35 +1,58 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".container");
+  //paddle
+  const paddle = document.getElementsByClassName("paddle");
+  let paddleY = 0;
+  let dpy = 10;
+  //movement of paddle with up or down arrow
+  document.addEventListener("keydown", (event) => {
+    //stop default behaviour of up arrow key and down arrow key
+    event.preventDefault()
+    if (paddleY > 0 && event.keyCode == 38) {
+      //up
+      paddleY = paddleY + -1 * dpy;
+    } else if (paddleY < 400 && event.keyCode == 40) {
+      //down
+      paddleY = paddleY + dpy;
+    }
 
-document.addEventListener('DOMContentLoaded',()=>{ 
-    const container=document.querySelector('.container')
-   //paddle    
-const paddle=document.getElementsByClassName('paddle')  
-let paddleY=1
-let dy=1
-const paddleMovement=()=>{
-    (paddleY<400 && paddleY>0) ? dy*=1 : dy*=-1
-    paddleY=paddleY+dy
-    paddle[0].style.top=`${paddleY}px`
-}
-setInterval(paddleMovement,0.1)
+    paddle[0].style.top = `${paddleY}px`;
+  });
+// // movement of paddle with mouse
+// document.addEventListener('mousemove',(event)=>{
+//     // console.log('event ',event)
+//     if(event.clientX > container.offsetLeft+container.offsetWidth/2) return
+//     let mouseDistanceFromTop=event.clientY
+//     let distanceOfContainerFromTop=container.offsetTop;
+//     let mousePointControl=mouseDistanceFromTop-distanceOfContainerFromTop-paddle.offsetHeight/2
+//     paddleY=mousePointControl
+//     if(paddleY<=0 || paddleY>container.offsetHeight-paddle[0].offsetHeight) return 
+//     paddle[0].style.top = `${paddleY}px`;
+// })
+  //ball
+  const ball = document.querySelector(".ball");
+  // console.log(ball.style.left,'ball')
+  let ballx = 25;
+  let bally = 15;
+  let dx = 1;
+  let dy = 1;
+  ball.style.left = `${ballx}px`;
+  ball.style.top = `${bally}px`;
+  const containerWidth = container.offsetWidth;
+  const containerHeight = container.offsetHeight;
+//   console.log('ball.offsetHeight',container.offsetTop)
 
-//ball
-const ball=document.querySelector('.ball')
-// console.log(ball.style.left,'ball')
-let ballx=15;
-let bally=1;
-let dx=1
-let dy_ball=1
-const containerWidth=container.offsetWidth
-    console.log(containerWidth,'width')
-const ballMovement=()=>{
-    (ballx<containerWidth-15 && ballx>=15) ? dx*=1 : dx*=-1
-    (bally<500-20 && bally>0) ? dy_ball*=1 : dy_ball*=-1
-    ballx+=dx
-    bally+=dy_ball
-ball.style.left=`${ballx}px`;
-ball.style.top=`${bally}px`;
-
-}
-setInterval(ballMovement,1)
-
-})
+  const ballMovement = () => {
+    ballx += dx;
+    bally += dy;
+    ball.style.left = `${ballx}px`;
+    ball.style.top = `${bally}px`;
+    //collison of ball and paddle
+    if(ballx<=paddle[0].offsetWidth &&
+     bally>paddleY && 
+     bally+ball.offsetHeight<paddleY+paddle[0].offsetHeight) dx*=-1
+    if (ballx > containerWidth - paddle[0].offsetWidth || ballx <= 0) dx *= -1;
+    bally < containerHeight -ball.offsetHeight  && bally > 0 ? (dy *= 1) : (dy *= -1);
+  };
+  setInterval(ballMovement, 1);
+});
